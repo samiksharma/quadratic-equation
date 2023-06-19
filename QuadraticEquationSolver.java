da420e92a2d55e2f288aee02e5e1a9957933cd1a
Samiksha Sharma <98591338+samiksharma@users.noreply.github.com>
2023-06-19 13:02:42 +0530

    Add files via upload
    A Java program that prints all real solutions to the quadratic equation ax2+bx+c = 0. If the discriminant is negative, displays a message stating that there are no real solutions.

---
+++ QuadraticEquationSolver.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class QuadraticEquationSolver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Enter the value of b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Enter the value of c: ");
+        double c = scanner.nextDouble();
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+            System.out.println("The quadratic equation has two real solutions:");
+            System.out.println("Root 1: " + root1);
+            System.out.println("Root 2: " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("The quadratic equation has one real solution:");
+            System.out.println("Root: " + root);
+        } else {
+            System.out.println("The quadratic equation has no real solutions.");
+        }
+
+        scanner.close();
+    }
+}
